a06c483e657aa95448ae11b0fd6d794860824717
Farzana Kabir Rinky <farzana.kabir@softbakso.com>
2019-09-08 19:06:23 +0600

    comiittttttt

--- src/app/home/find-our-cities/city.service.ts
+++ src/app/home/find-our-cities/city.service.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import {Injectable} from '@angular/core';
+=======
+import { Injectable } from '@angular/core';
+>>>>>>> a083650... find our cities and saying about us section is implemented
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
@@ -7,5 +11,9 @@ import {HttpClient} from '@angular/common/http';
 export class CityService {
 
   constructor() {
+<<<<<<< HEAD
+=======
+    private: HttpClient,
+>>>>>>> a083650... find our cities and saying about us section is implemented
   }
 }
