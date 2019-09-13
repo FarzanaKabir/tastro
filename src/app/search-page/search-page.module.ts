381324df33d3d7a279313b1898cf09a686588cc4
Farzana Kabir Rinky <farzana.kabir@softbakso.com>
2019-09-13 18:29:33 +0600

    restaurant details dynamic css implementation

--- src/app/search-page/search-page.module.ts
+++ src/app/search-page/search-page.module.ts
@@ -4,6 +4,12 @@ import { SearchPageComponent } from './search-page.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { SearchNotResultComponent } from './search-not-result/search-not-result.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {AppRoutingModule} from '../app-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -14,7 +20,13 @@ import { SearchNotResultComponent } from './search-not-result/search-not-result.
     NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ]
 })
 export class SearchPageModule { }
