b09c14a4d6ee722fd89604a6e86a3b065069a11b
Farzana Kabir Rinky <farzana.kabir@softbakso.com>
2019-09-19 18:49:35 +0600

    list component implimentation

--- src/app/restaurant-details/details/details.component.ts
+++ src/app/restaurant-details/details/details.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {RestaurantDetailsService} from '../restaurant-details.service';
-import {FoodItem} from '../food-item';
-
 
 @Component({
   selector: 'app-details',
@@ -9,78 +6,64 @@ import {FoodItem} from '../food-item';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-  promotions: FoodItem[] = [
-    {
-      id: 0,
-      image: '../../../assets/restaurant/photo1.png',
-      title: 'Beef Taco Subi',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-    {
-      id: 1,
-      image: '../../../assets/restaurant/photo2.png',
-      title: 'Chikepia Quinoa Subi',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-    {
-      id: 2,
-      image: '../../../assets/restaurant/photo3.png',
-      title: 'Chikepia Quinoa Subi Salad',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-    {
-      id: 3,
-      image: '../../../assets/restaurant/photo4.png',
-      title: 'Remix Subi',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-  ];
-  subi_meals: FoodItem[] = [
-    {
-      id: 4,
-      image: '../../../assets/restaurant/photo5.png',
-      title: 'Beef Taco Subi',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-    {
-      id: 5,
-      image: '../../../assets/restaurant/photo6.png',
-      title: 'Beef Taco Subi',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-    {
-      id: 6,
-      image: '../../../assets/restaurant/photo7.png',
-      title: 'Beef Taco Subi',
-      text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
-      emoji: '../../../assets/restaurant/happy.png'
-    },
-  ];
+  // promotions: FoodItem[] = [
+  //   {
+  //     id: 0,
+  //     image: '../../../assets/restaurant/photo1.png',
+  //     title: 'Beef Taco Subi',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  //   {
+  //     id: 1,
+  //     image: '../../../assets/restaurant/photo2.png',
+  //     title: 'Chikepia Quinoa Subi',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  //   {
+  //     id: 2,
+  //     image: '../../../assets/restaurant/photo3.png',
+  //     title: 'Chikepia Quinoa Subi Salad',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  //   {
+  //     id: 3,
+  //     image: '../../../assets/restaurant/photo4.png',
+  //     title: 'Remix Subi',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  // ];
+  // subi_meals: FoodItem[] = [
+  //   {
+  //     id: 4,
+  //     image: '../../../assets/restaurant/photo5.png',
+  //     title: 'Beef Taco Subi',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  //   {
+  //     id: 5,
+  //     image: '../../../assets/restaurant/photo6.png',
+  //     title: 'Beef Taco Subi',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  //   {
+  //     id: 6,
+  //     image: '../../../assets/restaurant/photo7.png',
+  //     title: 'Beef Taco Subi',
+  //     text: 'The new Taco Subi is filled with tasty Taco-flavored chicken of beef mince, crispy corn chips and the whole is crowned with Salsa sauce',
+  //     emoji: '../../../assets/restaurant/happy.png'
+  //   },
+  // ];
 
-  constructor(
-    // public restApi: RestaurantDetailsService
-  ) {
+  constructor() {
   }
 
   ngOnInit() {
-    // this.getFoodItems();
   }
 
-  // getFoodItems() {
-  //   this.Item = [];
-  //   this.restApi.getFoodItems().subscribe((data: {}) => {
-  //     console.log(data);
-  //     this.Item = data;
-  //   });
-  // }
-  //
-  // getImage(type: string = 'foodItem', image: string): string {
-  //   return 'https://soft-box.s3.us-east-2.amazonaws.com/' + type + '/' + image;
-  // }
 }
--- src/app/search-page/search-result/item-list/item-list.component.ts
+++ src/app/search-page/search-result/item-list/item-list.component.ts
@@ -1,18 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-
-export interface ItemList {
-  id: number;
-  image: string;
-  name: string;
-  rating: string;
-  country: string;
-}
-
-export interface tagList {
-  button1: string;
-  button2: string;
-  button3: string;
-}
+import {ConfigService} from '../../../service/config.service';
 
 @Component({
   selector: 'app-item-list',
@@ -20,63 +7,24 @@ export interface tagList {
   styleUrls: ['./item-list.component.scss']
 })
 export class ItemListComponent implements OnInit {
-
-  foodItem: ItemList[] = [
-    {
-      id: 0,
-      image: '../../../assets/search/Image1.png',
-      name: 'Pizza Hut',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    },
-    {
-      id: 1,
-      image: '../../../assets/search/Image2.png',
-      name: 'Hundi',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    },
-    {
-      id: 2,
-      image: '../../../assets/search/Image1.png',
-      name: 'Hundi',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    },
-    {
-      id: 3,
-      image: '../../../assets/search/Image1.png',
-      name: 'Pizza Hut',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    },
-    {
-      id: 4,
-      image: '../../../assets/search/Image2.png',
-      name: 'Hundi',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    },
-    {
-      id: 5,
-      image: '../../../assets/search/Image1.png',
-      name: 'Pizza Hut',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    },
-    {
-      id: 6,
-      image: '../../../assets/search/Image1.png',
-      name: 'Hundi',
-      rating: '125',
-      country: 'Mämminiementie 69, TURKU',
-    }
-  ];
-
-  constructor() {
+  private Item: {};
+  constructor(public restApi: ConfigService) {
   }
 
   ngOnInit() {
+    this.getFooItems();
+  }
+
+  getFooItems() {
+    this.Item = [];
+    this.restApi.getFoodItems().subscribe((data: {}) => {
+      console.log(data);
+      this.Item = data;
+    });
+  }
+
+  getImage(type: string = 'foodItem', image: string): string {
+    return 'https://soft-box.s3.us-east-2.amazonaws.com/' + type + '/' + image;
   }
 
 }
